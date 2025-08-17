3b3c9d8755f80d8cb5f9f55f6765d2ae3150263a
Benoit Havet <myungjae84@gmail.com>
2025-08-17 16:54:10 +0200

    add logo in head

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,16 +1,17 @@
-"use client"
+"use client";
 
 import classNames from "classnames/bind";
 import React from "react";
 import styles from "./Head.module.scss";
 import { Nav } from "../Nav/Nav";
+import Logo from "../Logo/Logo";
 
 const cx = classNames.bind(styles);
 
 export const Head = () => {
   return (
     <header className={cx("header")}>
-      <h1>Hb studio</h1>
+      <Logo src="/images/HBstudiologo.png" />
       <Nav />
     </header>
   );
